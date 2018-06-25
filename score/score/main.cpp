1b2ccfa5d24f18b27698d3ef2b1526124423b6af
Summer4580 <1844496975@qq.com>
2018-06-25 11:32:23 +0800

    4580

---
+++ score/score/main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  score
+//
+//  Created by 4580summer on 18/6/25.
+//  Copyright © 2018年 4580summer. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+struct player{
+    std::string name;
+    std::string sex;
+    std::string Class;
+};
+struct player_sort{
+    
+};
+struct jury{
+    std::string name;
+    std::string sex;
+    double tel;
+};
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
